dc0dbdc020a5404aa3fd8791ec788f2e461da04b
santhoshnc <ncsanthosh7@gmail.com>
2023-01-16 00:24:26 +0530

    Added the code for solution

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,16 +1,17 @@
 terraform {
-   backend "s3" {
-     bucket = "udacity-tf-travis-west"
-     key    = "terraform/terraform.tfstate"
-     region = "us-west-1"
-   }
- }
+  backend "s3" {
+    bucket = "udacity-tf-santhoshnc1"
+    key    = "terraform/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
 
- provider "aws" {
-   region = "us-west-1"
-   #profile = "default"
-   
-   default_tags {
-     tags = local.tags
-   }
- }
+provider "aws" {
+  region = "us-west-1"
+  #alias  = "usw1"
+  #profile = "default"
+
+  default_tags {
+    tags = local.tags
+  }
+}
